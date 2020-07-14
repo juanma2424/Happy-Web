002a1040fceb60ff3311734a7fa97dac4e01613b
juanma <42756436+juanma2424@users.noreply.github.com>
2020-07-14 05:20:25 -0600

    data pp

--- js/data.js
+++ js/data.js
@@ -1,9 +1,11 @@
-Highcharts.getJSON( 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population.json',function (data) {
+Highcharts.getJSON('https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/Data/JSData/MM2019.json', function (data) {
+
     Highcharts.mapChart('container', {
         chart: {
             borderWidth: 1,
             map: 'custom/world'
         },
+
         title: {
             text: 'World population 2013 by country'
         },
@@ -33,9 +35,9 @@ Highcharts.getJSON( 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/sa
             joinBy: ['iso-a3', 'code3'],
             data: data,
             minSize: 4,
-            maxSize: '12%',
+            maxSize: '7%',
             tooltip: {
-                pointFormat: '{point.properties.hc-a2}:{point.z} thousands'
+                pointFormat: '{point.properties.hc-a2}: {point.z} Happy Score'
             }
         }]
     });
