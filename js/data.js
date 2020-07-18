9be53a2502958dd56dffdfa4dd7e312e0c0e4add
juanma <42756436+juanma2424@users.noreply.github.com>
2020-07-18 14:19:43 -0600

    new data bar

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
 var pathMap;
 var pathPie;
 var pathSemiPie;
+var pathBar;
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
@@ -13,22 +14,31 @@ slider.oninput = function () {
     if (slider.value === "2015") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2015M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2015P.json';
+        pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2015SP.json';
     }
     if (slider.value === "2016") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2016M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2016P.json';
+        pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2016SP.json';
+
     }
     if (slider.value === "2017") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2017M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2017P.json';
+        pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2017SP.json';
+
     }
     if (slider.value === "2018") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2018M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2018P.json';
+        pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2018SP.json';
+
     }
     if (slider.value === "2019") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2019M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2019P.json';
+        pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2019SP.json';
+
     }
 
     Highcharts.getJSON(pathMap, function (data) {
@@ -86,7 +96,7 @@ slider.oninput = function () {
                 type: 'pie'
             },
             title: {
-                text: 'health in countries'
+                text: 'Health in countries'
             },
             tooltip: {
                 pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -107,7 +117,7 @@ slider.oninput = function () {
                 }
             },
             series: [{
-                name: 'Brands',
+                name: 'Health',
                 colorByPoint: true,
                 data: data
             }]
@@ -123,7 +133,7 @@ slider.oninput = function () {
                 plotShadow: false
             },
             title: {
-                text: 'Browser<br>shares<br>2017',
+                text: 'Freedom<br>in<br>countries',
                 align: 'center',
                 verticalAlign: 'middle',
                 y: 60
@@ -154,9 +164,61 @@ slider.oninput = function () {
             },
             series: [{
                 type: 'pie',
-                name: 'Browser share',
+                name: 'Freedom',
                 innerSize: '50%',
                 data: data
+            }]
+        });
+    })
+
+    Highcharts.getJSON(pathBar, function (data) {
+        Highcharts.chart('barcontainer', {
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'World\'s largest cities per 2017'
+            },
+            subtitle: {
+                text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
+            },
+            xAxis: {
+                type: 'category',
+                labels: {
+                    rotation: -45,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'Population in 2017: <b>{point.y:.1f} millions</b>'
+            },
+            series: [{
+                name: 'Population',
+                data:data,
+                dataLabels: {
+                    enabled: true,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    format: '{point.y:.1f}', // one decimal
+                    y: 10, // 10 pixels down from the top
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
             }]
         });
     })
