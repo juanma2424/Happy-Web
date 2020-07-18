4022ced6804757fc116df26fa4977941bdd5edd6
juanma <42756436+juanma2424@users.noreply.github.com>
2020-07-18 10:41:46 -0600

    new data

--- js/data.js
+++ js/data.js
@@ -60,43 +60,43 @@ slider.oninput = function () {
     });
 
 
-    Highcharts.getJSON('https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/Data/JSData/2019P.json', function (data) {
+    // Highcharts.getJSON('https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/Data/JSData/2019PG.json', function (data) {
 
-        Highcharts.chart('container1', {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'pie'
-            },
-            title: {
-                text: 'Browser market shares in January, 2018'
-            },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            accessibility: {
-                point: {
-                    valueSuffix: '%'
-                }
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-                    }
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data:data
-            }]
-        });
-    })
+    //     Highcharts.chart('container1', {
+    //         chart: {
+    //             plotBackgroundColor: null,
+    //             plotBorderWidth: null,
+    //             plotShadow: false,
+    //             type: 'pie'
+    //         },
+    //         title: {
+    //             text: 'Browser market shares in January, 2018'
+    //         },
+    //         tooltip: {
+    //             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    //         },
+    //         accessibility: {
+    //             point: {
+    //                 valueSuffix: '%'
+    //             }
+    //         },
+    //         plotOptions: {
+    //             pie: {
+    //                 allowPointSelect: true,
+    //                 cursor: 'pointer',
+    //                 dataLabels: {
+    //                     enabled: true,
+    //                     format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+    //                 }
+    //             }
+    //         },
+    //         series: [{
+    //             name: 'Brands',
+    //             colorByPoint: true,
+    //             data:data
+    //         }]
+    //     });
+    // })
 
 }
 
