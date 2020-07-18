48b2dca56e56dc4b44dd5b2c4bab1663d11318fe
juanma <42756436+juanma2424@users.noreply.github.com>
2020-07-18 17:29:53 -0600

    END/FIN

--- js/data.js
+++ js/data.js
@@ -2,10 +2,201 @@ var pathMap;
 var pathPie;
 var pathSemiPie;
 var pathBar;
+
+
+
+var FpathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2015M.json';
+var FpathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2015P.json';
+var FpathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2015SP.json';
+var FpathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2015BAR.json';
+
+
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
+
+
+
+
+Highcharts.getJSON(FpathMap, function (data) {
+
+    // Prevent logarithmic errors in color calulcation
+    data.forEach(function (p) {
+        p.value = (p.value < 1 ? 1 : p.value);
+    });
+
+    // Initiate the chart
+    Highcharts.mapChart('container', {
+        chart: {
+            map: 'custom/world'
+        },
+
+        title: {
+            text: 'Happiest countries in the world 2015'
+        },
+
+        mapNavigation: {
+            enabled: true,
+            enableDoubleClickZoomTo: true
+        },
+
+        colorAxis: {
+            min: 400,
+            max: 800,
+            type: 'logarithmic'
+        },
+
+        series: [{
+            data: data,
+            joinBy: ['iso-a3', 'code3'],
+            name: 'Happiness',
+            states: {
+                hover: {
+                    color: '#a4edba'
+                }
+            },
+            tooltip: {
+                valueSuffix: '/km²'
+            }
+        }]
+    });
+});
+
+
+Highcharts.getJSON(FpathPie, function (data) {
+
+    Highcharts.chart('piecontainer', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Health in countries 2015'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+            name: 'Health',
+            colorByPoint: true,
+            data: data
+        }]
+    });
+})
+
+
+Highcharts.getJSON(FpathSemiPie, function (data) {
+    Highcharts.chart('semipiecontainer', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 0,
+            plotShadow: false
+        },
+        title: {
+            text: 'Freedom<br>in<br>countries<br>2015',
+            align: 'center',
+            verticalAlign: 'middle',
+            y: 60
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        accessibility: {
+            point: {
+                valueSuffix: '%'
+            }
+        },
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    distance: -50,
+                    style: {
+                        fontWeight: 'bold',
+                        color: 'white'
+                    }
+                },
+                startAngle: -90,
+                endAngle: 90,
+                center: ['50%', '75%'],
+                size: '110%'
+            }
+        },
+        series: [{
+            type: 'pie',
+            name: 'Freedom',
+            innerSize: '50%',
+            data: data
+        }]
+    });
+})
+
+Highcharts.getJSON(FpathBar, function (data) {
+    Highcharts.chart('barcontainer', {
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Happiest countries 2015'
+        },
+        xAxis: {
+            type: 'category',
+            labels: {
+                rotation: -45,
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Happieness'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        tooltip: {
+            pointFormat: 'Happiness Rank: <b>{point.y:.1f} millions</b>'
+        },
+        series: [{
+            name: 'Population',
+            data: data,
+            dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 10, // 10 pixels down from the top
+                style: {
+                    fontSize: '13px',
+                    fontFamily: 'Verdana, sans-serif'
+                }
+            }
+        }]
+    });
+})
 
 
 slider.oninput = function () {
@@ -19,14 +210,14 @@ slider.oninput = function () {
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2015SP.json';
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2015BAR.json';
         document.getElementById("myImg").src = "DATA/IMG/2015.png";
-       
-       
+
+
     }
     if (slider.value === "2016") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2016M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2016P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2016SP.json';
-        pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2016BAR.json';       
+        pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2016BAR.json';
         document.getElementById("myImg").src = "DATA/IMG/2016.png";
 
     }
@@ -51,9 +242,9 @@ slider.oninput = function () {
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2019BAR.json';
         document.getElementById("myImg").src = "DATA/IMG/2019.png";
     }
-   
-   
-   
+
+
+
 
 
 
@@ -72,7 +263,7 @@ slider.oninput = function () {
             },
 
             title: {
-                text: 'Happiest countries in the world'
+                text: 'Happiest countries in the world ' + slider.value
             },
 
             mapNavigation: {
@@ -113,7 +304,7 @@ slider.oninput = function () {
                 type: 'pie'
             },
             title: {
-                text: 'Health in countries'
+                text: 'Health in countries ' + slider.value
             },
             tooltip: {
                 pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -150,7 +341,7 @@ slider.oninput = function () {
                 plotShadow: false
             },
             title: {
-                text: 'Freedom<br>in<br>countries',
+                text: 'Freedom<br>in<br>countries<br>' + slider.value,
                 align: 'center',
                 verticalAlign: 'middle',
                 y: 60
@@ -194,7 +385,7 @@ slider.oninput = function () {
                 type: 'column'
             },
             title: {
-                text: 'Happiest countries'
+                text: 'Happiest countries ' + slider.value
             },
             xAxis: {
                 type: 'category',
