c0a4ea7507301590590ac737ba12ff4a1362edf6
juanma <42756436+juanma2424@users.noreply.github.com>
2020-07-18 17:11:47 -0600

    END

--- js/data.js
+++ js/data.js
@@ -2,53 +2,61 @@ var pathMap;
 var pathPie;
 var pathSemiPie;
 var pathBar;
-var img;
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
 
+
 slider.oninput = function () {
 
     output.innerHTML = this.value;
+    img = document.createElement("img");
 
     if (slider.value === "2015") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2015M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2015P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2015SP.json';
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2015BAR.json';
-        img.src = "C:/Users/USER/Desktop/PC-3/DATA/IMG/2019.png";
+        document.getElementById("myImg").src = "DATA/IMG/2015.png";
+       
        
     }
     if (slider.value === "2016") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2016M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2016P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2016SP.json';
-        pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2016BAR.json';
+        pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2016BAR.json';       
+        document.getElementById("myImg").src = "DATA/IMG/2016.png";
+
     }
     if (slider.value === "2017") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2017M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2017P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2017SP.json';
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2017BAR.json';
+        document.getElementById("myImg").src = "DATA/IMG/2017.png";
     }
     if (slider.value === "2018") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2018M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2018P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2018SP.json';
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2018BAR.json';
+        document.getElementById("myImg").src = "DATA/IMG/2018.png";
     }
     if (slider.value === "2019") {
         pathMap = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONMAP/2019M.json';
         pathPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONPIE/2019P.json';
         pathSemiPie = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/JSONSEMIPIE/2019SP.json';
         pathBar = 'https://raw.githubusercontent.com/juanma2424/Happy-Web/juanma/DATA/JSON/BARJSON/2019BAR.json';
+        document.getElementById("myImg").src = "DATA/IMG/2019.png";
     }
+   
+   
+   
 
 
-    img = document.createElement("img");
-    var src = document.getElementById("picture1");
-    src.appendChild(img);
+
 
     Highcharts.getJSON(pathMap, function (data) {
 
